e21ed5c745a4af268612be6848920536e345066c
egolol <egolol87@gmail.com>
2023-06-19 12:04:58 +0200

    CorePeriphery and Richness

---
+++ braph2genesis/measures/_Richness.gen.m
@@ -0,0 +1,243 @@
+%% ¡header!
+Richness < Degree (m, richness) is the graph richness.
+
+%%% ¡description!
+The richness of a node is the sum of the edges that connect nodes 
+of higher degree within a layer.
+
+%% ¡props_update!
+
+%%% ¡prop!
+NAME (constant, string) is the name of the richness.
+%%%% ¡default!
+'Richness'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the richness.
+%%%% ¡default!
+'The richness of a node is the sum of the edges that connect nodes of higher degree within a layer.'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the richness.
+
+%%% ¡prop!
+ID (data, string) is a few-letter code of the richness.
+%%%% ¡default!
+'Richness ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the richness.
+%%%% ¡default!
+'Richness label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the richness.
+%%%% ¡default!
+'Richness notes'
+
+%%% ¡prop!
+SHAPE (constant, scalar) is the measure shape __Measure.NODAL__.
+%%%% ¡default!
+Measure.NODAL
+
+%%% ¡prop!
+SCOPE (constant, scalar) is the measure scope __Measure.UNILAYER__.
+%%%% ¡default!
+Measure.UNILAYER
+
+%%% ¡prop!
+PARAMETRICITY (constant, scalar) is the parametricity of the measure __Measure.NONPARAMETRIC__.
+%%%% ¡default!
+Measure.NONPARAMETRIC
+
+%%% ¡prop!
+COMPATIBLE_GRAPHS (constant, classlist) is the list of compatible graphs.
+%%%% ¡default!
+{'GraphWU' 'GraphWD' 'GraphBU' 'GraphBD' 'MultigraphBUD' 'MultigraphBUT' 'MultiplexWU' 'MultiplexWD' 'MultiplexBU' 'MultiplexBD' 'MultiplexBUD' 'MultiplexBUT'} ;
+
+%%% ¡prop!
+M (result, cell) is the richness.
+%%%% ¡calculate!
+g = m.get('G'); % graph from measure class
+A = g.get('A'); % cell with adjacency matrix (for graph) or 2D-cell array (for multigraph, multiplex, etc.)
+L = g.get('LAYERNUMBER');
+
+richness = cell(L, 1);
+directionality_type = g.get('DIRECTIONALITY_TYPE', L); 
+
+for li = 1:1:L
+    
+    Aii = A{li, li};
+    directionality_layer = directionality_type(li, li);
+
+    if directionality_layer == Graph.UNDIRECTED  % undirected graphs
+        
+        degree = calculateValue@Degree(m, prop);    
+        k = degree{li};
+        
+    else  % directed graphs
+                
+        in_degree = DegreeIn('G', g).get('M');        
+        out_degree = DegreeOut('G', g).get('M');        
+        
+        k = in_degree{li} + out_degree{li};
+    end
+    
+    kMinus = zeros(size(k));
+    kPlus = zeros(size(k));
+    
+    for i = 1:N(li)
+        lrInd = k <= k(i); % Indices of nodes with Lower Richness (LR)
+        hrInd = k > k(i); % Indices of nodes with Higher Richness (HR)
+        lrA = Aii;
+        lrA(i, hrInd) = 0;
+        lrA(hrInd, i) = 0;
+        hrA = Aii;
+        hrA(i, lrInd) = 0;
+        hrA(lrInd, i) = 0;
+        
+        if directionality_layer == Graph.UNDIRECTED  % undirected graphs
+            kMinusForI = sum(lrA, 1)';
+            kPlusForI = sum(hrA, 1)';
+        else
+            kMinusForI = (sum(lrA, 1)' + sum(lrA, 2))/2;
+            kPlusForI = (sum(hrA, 1)' + sum(hrA, 2))/2;
+        end
+        kMinus(i) = kMinusForI(i);
+        kPlus(i) = kPlusForI(i);
+    end
+    richness(li) = {kPlus};  % add richness of layer li
+end
+
+value = richness;
+
+%% ¡tests!
+
+%%% ¡excluded_props!
+[Richness.PFM]
+
+%%% ¡test!
+%%%% ¡name!
+GraphBU
+%%%% ¡code!
+B = [
+    0  1  1  0; 
+    1  0  1  1; 
+    1  1  0  0;
+    0  1  0  0
+    ];
+
+known_richness = {[1 0 1 1]'};
+
+g = GraphBU('B', B);
+
+richness = Richness('G', g).get('M');
+
+assert(isequal(richness, known_richness), ...
+    [BRAPH2.STR ':Richness:' BRAPH2.BUG_ERR], ...
+    'Richness is not being calculated correctly for GraphBU.')
+
+%%% ¡test!
+%%%% ¡name!
+MultigraphBUT
+%%%% ¡code!
+B = [
+    0  1  1  0; 
+    1  0  1  1; 
+    1  1  0  0;
+    0  1  0  0
+    ];
+
+known_richness = {
+                 [1 0 1 1]'
+                 [0 0 0 0]'};
+
+g = MultigraphBUT('B', B, 'THRESHOLDS', [0 1]);
+richness = Richness('G', g).get('M');
+
+assert(isequal(richness, known_richness), ...
+    [BRAPH2.STR ':Richness:' BRAPH2.BUG_ERR], ...
+    'Richness is not being calculated correctly for MultigraphBUT.')
+
+%%% ¡test!
+%%%% ¡name!
+GraphBD
+%%%% ¡code!
+A = [
+    0  1  1  1; 
+    1  0  1  1; 
+    1  1  0  0;
+    0  1  0  0
+    ];
+
+known_richness = {[1 0 2 3/2]'};
+
+g = GraphBD('B', A);
+richness = Richness('G', g).get('M');
+
+assert(isequal(richness, known_richness), ...
+    [BRAPH2.STR ':Richness:' BRAPH2.BUG_ERR], ...
+    'Richness is not being calculated correctly for GraphBD.')
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexBD
+%%%% ¡code!
+A11 = [
+    0  1  1  1; 
+    1  0  1  1; 
+    1  1  0  0;
+    0  1  0  0
+    ];
+
+A22 = [
+    0  1  1  1; 
+    1  0  1  1; 
+    1  1  0  0;
+    0  1  1  0
+    ];
+A = {A11 A22};
+             
+known_richness = {
+                 [1 0 2 3/2]'
+                 [1 0 1 2]'
+                 };    
+
+g = MultiplexBD('B', A);
+richness = Richness('G', g).get('M');
+
+assert(isequal(richness, known_richness), ...
+    [BRAPH2.STR ':Richness:' BRAPH2.BUG_ERR], ...
+    'Richness is not being calculated correctly for MultiplexBD.')
+
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexWD
+%%%% ¡code!
+A11 = [
+    0   1   1  .1; 
+    .2  0   1  1; 
+    1   1   0  0;
+    0   .3  0  0
+    ];
+A22 = [
+    0   1   1   .1; 
+    .2  0   1   1; 
+    1   1   0   0;
+    0   .3  .7  0
+    ];
+A = {A11 A22};
+
+known_richness = {
+                 [.6 0 2 .7]'
+                 [.6 0 1 21/20]'
+                 };   
+
+g = MultiplexWD('B', A);
+richness = Richness('G', g).get('M');
+richness = cellfun(@(s) round(s, 4), richness, 'UniformOutput', false);
+
+assert(isequal(richness, known_richness), ...
+    [BRAPH2.STR ':Richness:' BRAPH2.BUG_ERR], ...
+    'Richness is not being calculated correctly for MultiplexWD.')
