7035da2432b8ba3d19cc83f6f65427ead9b00c00
Yu-Wei Chang <hejyuwei@gmail.com>
2023-08-06 21:12:32 +0200

    u

---
+++ braph2genesis/neuralnetworks/_NNClassifierMLP.gen.m
@@ -0,0 +1,186 @@
+%% ¡header!
+NNClassifierMLP < NNBase (nn, multi-layer perceptron classifier) comprises a multi-layer perceptron classifier model and a list of given datasets.
+
+%%% ¡description!
+A neural network multi-layer perceptron classifier (NNClassifierMLP) comprises a multi-layer perceptron classifier model and a list of given datasets.
+NNClassifierMLP trains the multi-layer perceptron classifier with a formatted inputs ("CB", channel and batch) derived from the given datasets.
+
+%%% ¡seealso!
+NNDataPoint_CON_REG, NNRegressorMLP
+
+%% ¡props_update!
+
+%%% ¡prop!
+NAME (constant, string) is the name of the neural network multi-layer perceptron classifier.
+%%%% ¡default!
+'NNClassifierMLP'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the neural network multi-layer perceptron classifier.
+%%%% ¡default!
+'A neural network multi-layer perceptron regressor (NNRegressorMLP) comprises a multi-layer perceptron classifier model and a list of given datasets. NNclassifierMLP trains the multi-layer perceptron classifier with a formatted inputs ("CB", channel and batch) derived from the given datasets.'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the neural network multi-layer perceptron regressor.
+%%%% ¡settings!
+'NNClassifierMLP'
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the neural network multi-layer perceptron regressor.
+%%%% ¡default!
+'NNClassifierMLP ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the neural network multi-layer perceptron regressor.
+%%%% ¡default!
+'NNClassifierMLP label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the neural network multi-layer perceptron regressor.
+%%%% ¡default!
+'NNClassifierMLP notes'
+    
+%%% ¡prop!
+DATA_FORMAT (data, string) specifies the format of the data that flows through the neural network model. In this case, it maps "CB" (channel and batch) data.
+%%%% ¡default!
+'CB'
+
+%%% ¡prop!
+DATA_CONSTRUCT (query, cell) constructs the data based on the data format that flows through the neural network model.
+%%%% ¡calculate!
+inputs = nn.get('D').get('INPUTS');
+if isempty(inputs)
+    value = {};
+else
+    data = [];
+    for i = 1:1:length(inputs)
+        input = cell2mat(inputs{i});
+        data = [data; input(:)'];
+    end
+    value = {data};
+end
+
+%%% ¡prop!
+RESPONSE_CONSTRUCT (query, cell) constructs the responses based on the data format that flows through the neural network model.
+%%%% ¡calculate!
+targets = nn.get('D').get('TARGETS');
+if isempty(targets)
+    value = {};
+else
+    response = [];
+    for i = 1:1:length(targets)
+        target = cell2mat(targets{i});
+        response = [response; target(:)'];
+    end
+    value = {response};
+end
+
+%%% ¡prop!
+MODEL (result, net) is a trained neural network model with the given dataset.
+%%%% ¡calculate!
+data = cell2mat(nn.get('DATA_CONSTRUCT'));
+responses = cell2mat(nn.get('RESPONSE_CONSTRUCT'));
+if isempty(data) || isempty(responses)
+    value = network();
+else
+    data_format = nn.get('DATA_FORMAT');
+    ind_channel = find(data_format == 'C');
+    size_data = size(data);
+    num_features = size_data(end);
+    size_targets = size(responses);
+    num_responses = size_targets(end);
+    numLayers = nn.get('DENSE_LAYERS');
+    layers = [featureInputLayer(num_features, 'Name', 'input')];
+    for i = 1:1:length(numLayers)
+        layers = [layers
+            fullyConnectedLayer(numLayers(i), 'Name', ['fc' num2str(i)])
+            batchNormalizationLayer('Name', ['batchNormalization' num2str(i)])
+            dropoutLayer('Name', ['dropout' num2str(i)])
+            ];
+    end
+    layers = [layers
+        reluLayer('Name', 'relu1')
+        fullyConnectedLayer(num_responses, 'Name', 'fc_output')
+        classificationLayer('Name', 'output')
+        ];
+
+    % specify trianing options
+    if nn.get('PLOT_TRAINING')
+        plot_training = 'training-progress';
+    else
+        plot_training = 'none';
+    end
+
+    options = trainingOptions(nn.get('SOLVER'), ...
+        'MiniBatchSize', nn.get('BATCH'), ...
+        'MaxEpochs', nn.get('EPOCHS'), ...
+        'Shuffle', nn.get('SHUFFLE'), ...
+        'Plots', plot_training, ...
+        'Verbose', nn.get('VERBOSE'));
+
+    % train the neural network
+    value = trainNetwork(data, responses, layers, options);
+end
+
+%% ¡props!
+
+%%% ¡prop!
+DENSE_LAYERS (data, rvector) is user defined neural network layers.
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+train the classifier with example data
+%%%% ¡code!
+
+% % % % ensure the example data is generated
+% % % if ~isfile([fileparts(which('NNDataPoint_CON_REG')) filesep 'Example data NN REG CON XLS' filesep 'atlas.xlsx'])
+% % %     test_NNDataPoint_CON_REG % create example files
+% % % end
+% % % 
+% % % % Load BrainAtlas
+% % % im_ba = ImporterBrainAtlasXLS( ...
+% % %     'FILE', [fileparts(which('NNDataPoint_CON_REG')) filesep 'Example data NN REG CON XLS' filesep 'atlas.xlsx'], ...
+% % %     'WAITBAR', true ...
+% % %     );
+% % % 
+% % % ba = im_ba.get('BA');
+% % % 
+% % % % Load Groups of SubjectCON
+% % % im_gr = ImporterGroupSubjectCON_XLS( ...
+% % %     'DIRECTORY', [fileparts(which('NNDataPoint_CON_REG')) filesep 'Example data NN REG CON XLS' filesep 'CON_Group_XLS'], ...
+% % %     'BA', ba, ...
+% % %     'WAITBAR', true ...
+% % %     );
+% % % 
+% % % gr = im_gr.get('GR');
+% % % 
+% % % % create a item list of NNDataPoint_CON_REG
+% % % it_list = cellfun(@(x) NNDataPoint_CON_REG( ...
+% % %     'ID', x.get('ID'), ...
+% % %     'SUB', x, ...
+% % %     'TARGET_IDS', x.get('VOI_DICT').get('KEYS')), ...
+% % %     gr.get('SUB_DICT').get('IT_LIST'), ...
+% % %     'UniformOutput', false);
+% % % 
+% % % % create a NNDataPoint_CON_REG DICT
+% % % dp_list = IndexedDictionary(...
+% % %         'IT_CLASS', 'NNDataPoint_CON_REG', ...
+% % %         'IT_LIST', it_list ...
+% % %         );
+% % % 
+% % % % create a NNData containing the NNDataPoint_CON_REG DICT
+% % % d = NNDataset( ...
+% % %     'DP_CLASS', 'NNDataPoint_CON_REG', ...
+% % %     'DP_DICT', dp_list ...
+% % %     );
+% % % 
+% % % nn = NNRegressorMLP('D', d, 'DENSE_LAYERS', [20 20]);
+% % % trained_model = nn.get('MODEL');
+% % % 
+% % % % Check whether the number of fully-connected layer matches (excluding fc_output layer)
+% % % assert(length(nn.get('DENSE_LAYERS')) == sum(contains({trained_model.Layers.Name}, 'fc')) - 1, ...
+% % %     [BRAPH2.STR ':NNRegressorMLP:' BRAPH2.FAIL_TEST], ...
+% % %     'NNRegressorMLP does not construct the layers correctly. The number of the inputs should be the same as the length of dense layers the property.' ...
+% % %     )
---
+++ braph2genesis/neuralnetworks/_NNEvaluator_CLA.gen.m
@@ -0,0 +1,273 @@
+%% ¡header!
+NNEvaluator_CLA < NNEvaluator (nne, neural network evaluator for classification) evaluates the performance of a neural network classifier with a given dataset.
+
+%%% ¡description!
+A neural network evaluator for regression (NNEvaluator_CLA) evaluates the performance of a neural network regressor with a given dataset.
+NNEvaluator_CLA evaluates the performance of the trained regressor with a given dataset in terms of various regression metrics (e.g., coefficient of determination, mean squared error).
+
+%%% ¡seealso!
+NNDataPoint_CON_REG, NNRegressorMLP
+
+%% ¡props_update!
+
+%%% ¡prop!
+NAME (constant, string) is the name of the neural network evaluator for the regression task.
+%%%% ¡default!
+'NNEvaluator_CLA'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the neural network evaluator for the regression task.
+%%%% ¡default!
+'A neural network evaluator for regression (NNEvaluator_CLA) evaluates the performance of a neural network regressor with a given dataset. NNEvaluator_CLA evaluates the performance of the trained regressor with a given dataset in terms of various regression metrics (e.g., coefficient of determination, mean squared error).'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the neural network evaluator for the regression task.
+%%%% ¡settings!
+'NNEvaluator_CLA'
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the neural network evaluator for the regression task.
+%%%% ¡default!
+'NNEvaluator_CLA ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the neural network evaluator for the regression task.
+%%%% ¡default!
+'NNEvaluator_CLA label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the neural network evaluator for the regression task.
+%%%% ¡default!
+'NNEvaluator_CLA notes'
+    
+%%% ¡prop!
+NN (data, item) contains a trained neural network regressor.
+%%%% ¡settings!
+'NNRegressorMLP'
+
+%%% ¡prop!
+PREDICTIONS (result, cell) are the predictions of the trained neural network model from the given dataset.
+%%%% ¡calculate!
+nn = nne.get('NN');
+net = nn.get('MODEL');
+inputs = cell2mat(eval([nn.getClass char("('D', nne.get('D')).get('DATA_CONSTRUCT')")]));
+if isempty(inputs)
+    value = {};
+else
+    value = {net.predict(inputs)};
+end
+
+%% ¡props!
+
+%%% ¡prop!
+GROUND_TRUTH (query, matrix) returns the matrix of ground truth derived from the targets.
+%%%% ¡calculate!
+targets = nne.get('D').get('TARGETS');
+if isempty(targets)
+    value = [];
+else
+    for i = 1:length(targets)
+        value(i, :) = cell2mat(targets{i});
+    end
+end
+
+%%% ¡prop!
+CORRELATION_COEFF (result, rvector) provides the metric of the correlation of coefficients.
+%%%% ¡calculate!
+predictions = cell2mat(nne.get('PREDICTIONS'));
+if isempty(predictions)
+    value = [];
+else
+    ground_truth = nne.get('GROUND_TRUTH');
+    num_dp = size(ground_truth, 1);
+    num_target = size(ground_truth, 2);
+    for i = 1:num_target
+        corr_matrix = corrcoef(predictions(:, i), ground_truth(:, i));
+        value(i) = corr_matrix(1, 2);
+    end
+end
+
+%%% ¡prop!
+COEFF_OF_DETERMINATION (result, rvector) provides a measure of how well the predictions are replicated by the model.
+%%%% ¡calculate!
+predictions = cell2mat(nne.get('PREDICTIONS'));
+if isempty(predictions)
+    value = [];
+else
+    ground_truth = nne.get('GROUND_TRUTH');
+    num_dp = size(ground_truth, 1);
+    num_target = size(ground_truth, 2);
+    for i = 1:num_target
+        corr_matrix = corrcoef(predictions(:, i), ground_truth(:, i));
+        value(i) = corr_matrix(1, 2)^2;
+    end
+end
+
+%%% ¡prop!
+MEAN_ABSOLUTE_ERROR (result, rvector) provides the metric of the mean absolute error.
+%%%% ¡calculate!
+predictions = cell2mat(nne.get('PREDICTIONS'));
+if isempty(predictions)
+    value = [];
+else
+    ground_truth = nne.get('GROUND_TRUTH');
+    num_dp = size(ground_truth, 1);
+    num_target = size(ground_truth, 2);
+    for i = 1:num_target
+        value(i) = mean(abs(predictions(:, i) - ground_truth(:, i)));
+    end
+end
+
+%%% ¡prop!
+MEAN_SQUARED_ERROR (result, rvector) provides the metric of the mean squared error.
+%%%% ¡calculate!
+predictions = cell2mat(nne.get('PREDICTIONS'));
+if isempty(predictions)
+    value = [];
+else
+    ground_truth = nne.get('GROUND_TRUTH');
+    num_dp = size(ground_truth, 1);
+    num_target = size(ground_truth, 2);
+    for i = 1:num_target
+        value(i) = mean((predictions(:, i) - ground_truth(:, i)).^2);
+    end
+end
+
+%%% ¡prop!
+ROOT_MEAN_SQUARED_ERROR (result, rvector) provides the metric of the root mean squared error.
+%%%% ¡calculate!
+predictions = cell2mat(nne.get('PREDICTIONS'));
+if isempty(predictions)
+    value = [];
+else
+    ground_truth = nne.get('GROUND_TRUTH');
+    num_dp = size(ground_truth, 1);
+    num_target = size(ground_truth, 2);
+    for i = 1:num_target
+        value(i) = sqrt(mean((predictions(:, i) - ground_truth(:, i)).^2));
+    end
+end
+
+%%% ¡prop!
+PERMUTATION_FEATURE_IMPORTANCE (result, cell) assess the significance of each feature by randomly shuffling its values and measuring how much the performance of the model decreases.
+%%%% ¡calculate!
+value = {};
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+evaluate a regressor with the example data
+%%%% ¡code!
+
+% ensure the example data is generated
+if ~isfile([fileparts(which('NNDataPoint_CON_REG')) filesep 'Example data NN REG CON XLS' filesep 'atlas.xlsx'])
+    test_NNDataPoint_CON_REG % create example files
+end
+
+% Load BrainAtlas
+im_ba = ImporterBrainAtlasXLS( ...
+    'FILE', [fileparts(which('NNDataPoint_CON_REG')) filesep 'Example data NN REG CON XLS' filesep 'atlas.xlsx'], ...
+    'WAITBAR', true ...
+    );
+
+ba = im_ba.get('BA');
+
+% Load Groups of SubjectCON
+im_gr = ImporterGroupSubjectCON_XLS( ...
+    'DIRECTORY', [fileparts(which('NNDataPoint_CON_REG')) filesep 'Example data NN REG CON XLS' filesep 'CON_Group_XLS'], ...
+    'BA', ba, ...
+    'WAITBAR', true ...
+    );
+
+gr = im_gr.get('GR');
+
+% create a item list of NNDataPoint_CON_REG
+it_list = cellfun(@(x) NNDataPoint_CON_REG( ...
+    'ID', x.get('ID'), ...
+    'SUB', x, ...
+    'TARGET_IDS', x.get('VOI_DICT').get('KEYS')), ...
+    gr.get('SUB_DICT').get('IT_LIST'), ...
+    'UniformOutput', false);
+
+% create a NNDataPoint_CON_REG DICT
+dp_list = IndexedDictionary(...
+        'IT_CLASS', 'NNDataPoint_CON_REG', ...
+        'IT_LIST', it_list ...
+        );
+
+% create a NNData containing the NNDataPoint_CON_REG DICT
+d = NNDataset( ...
+    'DP_CLASS', 'NNDataPoint_CON_REG', ...
+    'DP_DICT', dp_list ...
+    );
+
+nn = NNRegressorMLP('D', d, 'DENSE_LAYERS', [20 20]);
+nn.get('MODEL_TRAIN');
+nne = NNEvaluator_CLA('NN', nn, 'D', d);
+
+predictions = cell2mat(nne.get('PREDICTIONS'));
+
+% Check whether the ground truth are derived as expected
+ground_truth = nne.get('GROUND_TRUTH');
+targets = d.get('TARGETS');
+
+for i = 1:size(ground_truth, 1)
+    check(i) = isequal(cell2mat(targets{i}), ground_truth(i, :));
+end
+assert(all(check), ...
+    [BRAPH2.STR ':NNEvaluator_CLA:' BRAPH2.FAIL_TEST], ...
+    'NNEvaluator_CLA does not calculate the ground truth correctly.' ...
+    )
+
+% Check whether the correlation coefficients are calculated as expected
+calculated_value = nne.get('CORRELATION_COEFF');
+for i = 1:size(ground_truth, 2)
+    corr_matrix = corrcoef(predictions(:, i), ground_truth(:, i));
+    known_value(i) = corr_matrix(1, 2);
+end
+assert(isequal(calculated_value, known_value), ...
+    [BRAPH2.STR ':NNEvaluator_CLA:' BRAPH2.FAIL_TEST], ...
+    'NNEvaluator_CLA does not calculate the correlation coefficients correctly.' ...
+    )
+
+% Check whether the correlation coefficients are calculated as expected
+calculated_value = nne.get('COEFF_OF_DETERMINATION');
+for i = 1:size(ground_truth, 2)
+    corr_matrix = corrcoef(predictions(:, i), ground_truth(:, i));
+    known_value(i) = corr_matrix(1, 2)^2;
+end
+assert(isequal(calculated_value, known_value), ...
+    [BRAPH2.STR ':NNEvaluator_CLA:' BRAPH2.FAIL_TEST], ...
+    'NNEvaluator_CLA does not calculate the coefficient of determination correctly.' ...
+    )
+
+% Check whether the mean absolute errors are calculated as expected
+calculated_value = nne.get('MEAN_ABSOLUTE_ERROR');
+for i = 1:size(ground_truth, 2)
+    known_value(i) = mean(abs(predictions(:, i) - ground_truth(:, i)));
+end
+assert(isequal(calculated_value, known_value), ...
+    [BRAPH2.STR ':NNEvaluator_CLA:' BRAPH2.FAIL_TEST], ...
+    'NNEvaluator_CLA does not calculate the mean absolute errors correctly.' ...
+    )
+
+% Check whether the mean squared errors are calculated as expected
+calculated_value = nne.get('MEAN_SQUARED_ERROR');
+for i = 1:size(ground_truth, 2)
+    known_value(i) = mean((predictions(:, i) - ground_truth(:, i)).^2);
+end
+assert(isequal(calculated_value, known_value), ...
+    [BRAPH2.STR ':NNEvaluator_CLA:' BRAPH2.FAIL_TEST], ...
+    'NNEvaluator_CLA does not calculate the mean squared errors correctly.' ...
+    )
+
+% Check whether the mean squared errors are calculated as expected
+calculated_value = nne.get('ROOT_MEAN_SQUARED_ERROR');
+for i = 1:size(ground_truth, 2)
+    known_value(i) = sqrt(mean((predictions(:, i) - ground_truth(:, i)).^2));
+end
+assert(isequal(calculated_value, known_value), ...
+    [BRAPH2.STR ':NNEvaluator_CLA:' BRAPH2.FAIL_TEST], ...
+    'NNEvaluator_CLA does not calculate the root mean squared errors correctly.' ...
+    )
