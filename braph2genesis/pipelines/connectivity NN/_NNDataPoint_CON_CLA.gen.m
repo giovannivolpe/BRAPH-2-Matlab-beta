3b8315ef944af4174e356beabc40349c8e1f898f
Yu-Wei Chang <hejyuwei@gmail.com>
2023-08-07 13:40:51 +0200

    u

--- braph2genesis/pipelines/connectivity NN/_NNDataPoint_CON_CLA.gen.m
+++ braph2genesis/pipelines/connectivity NN/_NNDataPoint_CON_CLA.gen.m
@@ -47,9 +47,9 @@ INPUT (result, cell) is the input value for this data point.
 value = {dp.get('SUB').get('CON')};
     
 %%% ¡prop!
-TARGET (result, cell) is the target value for this data point.
+TARGET (result, stringlist) is the target values for this data point.
 %%%% ¡calculate!
-value = cellfun(@(x) dp.get('SUB').get('VOI_DICT').get('IT', x).get('V'), dp.get('TARGET_IDS'), 'UniformOutput', false);
+value = dp.get('TARGET_IDS');
 
 %% ¡props!
 
@@ -59,7 +59,7 @@ SUB (data, item) is a subject with connectivity data.
 'SubjectCON'
 
 %%% ¡prop!
-TARGET_IDS (parameter, stringlist) is a list of variable-of-interest IDs to be used as regression targets.
+TARGET_IDS (parameter, stringlist) is a list of variable-of-interest IDs to be used as the class targets.
 					
 %% ¡tests!
 
@@ -68,9 +68,9 @@ TARGET_IDS (parameter, stringlist) is a list of variable-of-interest IDs to be u
 
 %%% ¡test!
 %%%% ¡name!
-Create example files for regression
+Create example files
 %%%% ¡code!
-data_dir = [fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN REG CON XLS'];
+data_dir = [fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN CLA CON XLS'];
 if ~isdir(data_dir)
     mkdir(data_dir);
 
@@ -89,21 +89,20 @@ if ~isdir(data_dir)
 
     sex_options = {'Female' 'Male'};
 
-    % Group 
-    K = 2; % degree (mean node degree is 2K)
-    beta = 0.3; % Rewiring probability
-    gr_name = 'CON_Group_XLS';
-    gr_dir = [data_dir filesep() gr_name];
-    mkdir(gr_dir);
-    vois = [
+    % Group 1
+    K1 = 2; % degree (mean node degree is 2K) - group 1
+    beta1 = 0.3; % Rewiring probability - group 1
+    gr1_name = 'CON_Group_1_XLS';
+    gr1_dir = [data_dir filesep() gr1_name];
+    mkdir(gr1_dir);
+    vois1 = [
         {{'Subject ID'} {'Age'} {'Sex'}}
         {{} {} cell2str(sex_options)}
         ];
-    for i = 1:1:100 % subject number
+    for i = 1:1:50 % subject number
         sub_id = ['SubjectCON_' num2str(i)];
-        % create WS graphs with random beta
-        beta(i) = rand(1);
-        h = WattsStrogatz(N, K, beta(i)); % create WS graph
+
+        h1 = WattsStrogatz(N, K1, beta1); % create two WS graph
         % figure(1) % Plot the two graphs to double-check
         % plot(h1, 'NodeColor',[1 0 0], 'EdgeColor',[0 0 0], 'EdgeAlpha',0.1, 'Layout','circle');
         % title(['Group 1: Graph with $N = $ ' num2str(N_nodes) ...
@@ -111,25 +110,55 @@ if ~isdir(data_dir)
         %     'Interpreter','latex')
         % axis equal
 
-        A = full(adjacency(h)); A(1:length(A)+1:numel(A)) = 0; % extract the adjacency matrix
-        r = 0 + (0.5 - 0) * rand(size(A)); diffA = A - r; A(A ~= 0) = diffA(A ~= 0); % make the adjacency matrix weighted
-        A = max(A, transpose(A)); % make the adjacency matrix symmetric
+        A1 = full(adjacency(h1)); A1(1:length(A1)+1:numel(A1)) = 0; % extract the adjacency matrix
+        r = 0 + (0.5 - 0)*rand(size(A1)); diffA = A1 - r; A1(A1 ~= 0) = diffA(A1 ~= 0); % make the adjacency matrix weighted
+        A1 = max(A1, transpose(A1)); % make the adjacency matrix symmetric
 
-        writetable(array2table(A), [gr_dir filesep() sub_id '.xlsx'], 'WriteVariableNames', false)
+        writetable(array2table(A1), [gr1_dir filesep() sub_id '.xlsx'], 'WriteVariableNames', false)
 
         % variables of interest
-        age_upperBound = 80;
-        age_lowerBound = 50;
-        age = age_lowerBound + beta(i)*(age_upperBound - age_lowerBound);
-        vois = [vois; {sub_id, age, sex_options(randi(2))}];
+        vois1 = [vois1; {sub_id, randi(90), sex_options(randi(2))}];
     end
-    writetable(table(vois), [data_dir filesep() gr_name '.vois.xlsx'], 'WriteVariableNames', false)
+    writetable(table(vois1), [data_dir filesep() gr1_name '.vois.xlsx'], 'WriteVariableNames', false)
+
+    % Group 2
+    K2 = 2; % degree (mean node degree is 2K) - group 2
+    beta2 = 0.85; % Rewiring probability - group 2
+    gr2_name = 'CON_Group_2_XLS';
+    gr2_dir = [data_dir filesep() gr2_name];
+    mkdir(gr2_dir);
+    vois2 = [
+        {{'Subject ID'} {'Age'} {'Sex'}}
+        {{} {} cell2str(sex_options)}
+        ];
+    for i = 51:1:100
+        sub_id = ['SubjectCON_' num2str(i)];
+
+        h2 = WattsStrogatz(N, K2, beta2);
+        % figure(2)
+        % plot(h2, 'NodeColor',[1 0 0], 'EdgeColor',[0 0 0], 'EdgeAlpha',0.1, 'Layout','circle');
+        % title(['Group 2: Graph with $N = $ ' num2str(N_nodes) ...
+        %     ' nodes, $K = $ ' num2str(K2) ', and $\beta = $ ' num2str(beta2)], ...
+        %     'Interpreter','latex')
+        % axis equal
+
+        A2 = full(adjacency(h2)); A2(1:length(A2)+1:numel(A2)) = 0;
+        r = 0 + (0.5 - 0)*rand(size(A2)); diffA = A2 - r; A2(A2 ~= 0) = diffA(A2 ~= 0);
+        A2 = max(A2, transpose(A2));
+
+        writetable(array2table(A2), [gr2_dir filesep() sub_id '.xlsx'], 'WriteVariableNames', false)
+
+        % variables of interest
+        vois2 = [vois2; {sub_id, randi(90), sex_options(randi(2))}];
+    end
+    writetable(table(vois2), [data_dir filesep() gr2_name '.vois.xlsx'], 'WriteVariableNames', false)
 
     % reset RNG
     rng(rng_settings_)
 end
+
 %%% ¡test_functions!
-function h = WattsStrogatz(N, K, beta)
+function h = WattsStrogatz(N,K,beta)
 % H = WattsStrogatz(N,K,beta) returns a Watts-Strogatz model graph with N
 % nodes, N*K edges, mean node degree 2*K, and rewiring probability beta.
 %
@@ -137,24 +166,24 @@ function h = WattsStrogatz(N, K, beta)
 
 % Connect each node to its K next and previous neighbors. This constructs
 % indices for a ring lattice.
-    s = repelem((1:N)', 1, K);
-    t = s + repmat(1:K, N, 1);
-    t = mod(t - 1, N) + 1;
+s = repelem((1:N)',1,K);
+t = s + repmat(1:K,N,1);
+t = mod(t-1,N)+1;
+
+% Rewire the target node of each edge with probability beta
+for source=1:N
+    switchEdge = rand(K, 1) < beta;
     
-    % Rewire the target node of each edge with probability beta
-    for source = 1:N
-        switchEdge = rand(K, 1) < beta;
-        
-        newTargets = rand(N, 1);
-        newTargets(source) = 0;
-        newTargets(s(t == source)) = 0;
-        newTargets(t(source, ~switchEdge)) = 0;
-        
-        [~, ind] = sort(newTargets, 'descend');
-        t(source, switchEdge) = ind(1:nnz(switchEdge));
-    end
+    newTargets = rand(N, 1);
+    newTargets(source) = 0;
+    newTargets(s(t==source)) = 0;
+    newTargets(t(source, ~switchEdge)) = 0;
     
-    h = graph(s,t);
+    [~, ind] = sort(newTargets, 'descend');
+    t(source, switchEdge) = ind(1:nnz(switchEdge));
+end
+
+h = graph(s,t);
 end
 
 %%% ¡test! 
@@ -163,57 +192,104 @@ Create a NNDataset containg NNDataPoint_CON_CLA with simulated data
 %%%% ¡code!
 % Load BrainAtlas
 im_ba = ImporterBrainAtlasXLS( ...
-    'FILE', [fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN REG CON XLS' filesep 'atlas.xlsx'], ...
+    'FILE', [fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN CLA CON XLS' filesep 'atlas.xlsx'], ...
     'WAITBAR', true ...
     );
 
 ba = im_ba.get('BA');
 
 % Load Groups of SubjectCON
-im_gr = ImporterGroupSubjectCON_XLS( ...
-    'DIRECTORY', [fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN REG CON XLS' filesep 'CON_Group_XLS'], ...
+im_gr1 = ImporterGroupSubjectCON_XLS( ...
+    'DIRECTORY', [fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN CLA CON XLS' filesep 'CON_Group_1_XLS'], ...
     'BA', ba, ...
     'WAITBAR', true ...
     );
 
-gr = im_gr.get('GR');
+gr1 = im_gr1.get('GR');
 
-% create a item list of NNDataPoint_CON_CLA
-it_list = cellfun(@(x) NNDataPoint_CON_CLA( ...
+im_gr2 = ImporterGroupSubjectCON_XLS( ...
+    'DIRECTORY', [fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN CLA CON XLS' filesep 'CON_Group_2_XLS'], ...
+    'BA', ba, ...
+    'WAITBAR', true ...
+    );
+
+gr2 = im_gr2.get('GR');
+
+% create item lists of NNDataPoint_CON_CLA
+[~, group_folder_name] = fileparts(im_gr1.get('DIRECTORY'));
+it_list1 = cellfun(@(x) NNDataPoint_CON_CLA( ...
     'ID', x.get('ID'), ...
     'SUB', x, ...
-    'TARGET_IDS', x.get('VOI_DICT').get('KEYS')), ...
-    gr.get('SUB_DICT').get('IT_LIST'), ...
+    'TARGET_IDS', {group_folder_name}), ...
+    gr1.get('SUB_DICT').get('IT_LIST'), ...
     'UniformOutput', false);
 
-% create a NNDataPoint_CON_CLA DICT
-dp_list = IndexedDictionary(...
+[~, group_folder_name] = fileparts(im_gr2.get('DIRECTORY'));
+it_list2 = cellfun(@(x) NNDataPoint_CON_CLA( ...
+    'ID', x.get('ID'), ...
+    'SUB', x, ...
+    'TARGET_IDS', {group_folder_name}), ...
+    gr2.get('SUB_DICT').get('IT_LIST'), ...
+    'UniformOutput', false);
+
+% create NNDataPoint_CON_CLA DICT items
+dp_list1 = IndexedDictionary(...
         'IT_CLASS', 'NNDataPoint_CON_CLA', ...
-        'IT_LIST', it_list ...
+        'IT_LIST', it_list1 ...
+        );
+
+dp_list2 = IndexedDictionary(...
+        'IT_CLASS', 'NNDataPoint_CON_CLA', ...
+        'IT_LIST', it_list2 ...
         );
 
 % create a NNDataset containing the NNDataPoint_CON_CLA DICT
-d = NNDataset( ...
+d1 = NNDataset( ...
     'DP_CLASS', 'NNDataPoint_CON_CLA', ...
-    'DP_DICT', dp_list ...
+    'DP_DICT', dp_list1 ...
+    );
+
+d2 = NNDataset( ...
+    'DP_CLASS', 'NNDataPoint_CON_CLA', ...
+    'DP_DICT', dp_list2 ...
     );
 
 % Check whether the number of inputs matches
-assert(length(d.get('INPUTS')) == gr.get('SUB_DICT').get('LENGTH'), ...
+assert(length(d1.get('INPUTS')) == gr1.get('SUB_DICT').get('LENGTH'), ...
 		[BRAPH2.STR ':NNDataPoint_CON_CLA:' BRAPH2.FAIL_TEST], ...
-		'NNDataPoint_CON_CLA does not construct the dataset correctly. The number of the inputs should be the same as the number of imported subjects.' ...
+		'NNDataPoint_CON_CLA does not construct the dataset correctly. The number of the inputs should be the same as the number of imported subjects of group 1.' ...
+		)
+
+assert(length(d2.get('INPUTS')) == gr2.get('SUB_DICT').get('LENGTH'), ...
+		[BRAPH2.STR ':NNDataPoint_CON_CLA:' BRAPH2.FAIL_TEST], ...
+		'NNDataPoint_CON_CLA does not construct the dataset correctly. The number of the inputs should be the same as the number of imported subjects of group 2.' ...
 		)
 
 % Check whether the number of targets matches
-assert(length(d.get('TARGETS')) == gr.get('SUB_DICT').get('LENGTH'), ...
+assert(length(d1.get('TARGETS')) == gr1.get('SUB_DICT').get('LENGTH'), ...
 		[BRAPH2.STR ':NNDataPoint_CON_CLA:' BRAPH2.FAIL_TEST], ...
-		'NNDataPoint_CON_CLA does not construct the dataset correctly. The number of the targets should be the same as the number of imported subjects.' ...
+		'NNDataPoint_CON_CLA does not construct the dataset correctly. The number of the targets should be the same as the number of imported subjects of group 1.' ...
+		)
+
+assert(length(d2.get('TARGETS')) == gr2.get('SUB_DICT').get('LENGTH'), ...
+		[BRAPH2.STR ':NNDataPoint_CON_CLA:' BRAPH2.FAIL_TEST], ...
+		'NNDataPoint_CON_CLA does not construct the dataset correctly. The number of the targets should be the same as the number of imported subjects of group 2.' ...
 		)
 
 % Check whether the content of input for a single datapoint matches
-for index = 1:1:gr.get('SUB_DICT').get('LENGTH')
-    individual_input = d.get('DP_DICT').get('IT', index).get('INPUT');
-    known_input = {gr.get('SUB_DICT').get('IT', index).get('CON')};
+for index = 1:1:gr1.get('SUB_DICT').get('LENGTH')
+    individual_input = d1.get('DP_DICT').get('IT', index).get('INPUT');
+    known_input = {gr1.get('SUB_DICT').get('IT', index).get('CON')};
+
+    assert(isequal(individual_input, known_input), ...
+        [BRAPH2.STR ':NNDataPoint_CON_CLA:' BRAPH2.FAIL_TEST], ...
+        'NNDataPoint_CON_CLA does not construct the dataset correctly. The input value is not derived correctly.' ...
+        )
+end
+
+for index = 1:1:gr2.get('SUB_DICT').get('LENGTH')
+    individual_input = d2.get('DP_DICT').get('IT', index).get('INPUT');
+    known_input = {gr2.get('SUB_DICT').get('IT', index).get('CON')};
 
     assert(isequal(individual_input, known_input), ...
         [BRAPH2.STR ':NNDataPoint_CON_CLA:' BRAPH2.FAIL_TEST], ...
@@ -225,9 +301,9 @@ end
 %%%% ¡name!
 Example
 %%%% ¡code!
-% ensure the example data is generated
-if ~isfile([fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN REG CON XLS' filesep 'atlas.xlsx'])
-    test_NNDataPoint_CON_CLA % create example files
-end
-
-example_NN_CON_Regression
+% % % % ensure the example data is generated
+% % % if ~isfile([fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN REG CON XLS' filesep 'atlas.xlsx'])
+% % %     test_NNDataPoint_CON_CLA % create example files
+% % % end
+% % % 
+% % % example_NN_CON_Classification
