dca314cefa2230fe27b81b4412924f945a32916d
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-08-08 15:59:49 +0200

    u

--- braph2genesis/src/nn/_NNBase.gen.m
+++ braph2genesis/src/nn/_NNBase.gen.m
@@ -1,5 +1,5 @@
 %% ¡header!
-NNBase < ConcreteElement (nn, neural network) comprises a neural network model with a given dataset.
+NNBase < ConcreteElement (nn, neural network) comprises a neural network model with a dataset.
 
 %%% ¡description!
 A neural network base (NNBase) comprises a neural network model with a specific dataset, along with the required options for training the model.
@@ -7,7 +7,7 @@ Instances of this class should not be created. Use one of its subclasses instead
 Its subclasses shall be specifically designed to cater to different use cases such as a classifier, a regressor, or a generative model.
 
 %%% ¡seealso!
-NNDataPoint, NNData, NNEvaluator
+NNDataPoint, NNDataset, NNEvaluator
 
 %% ¡props_update!
 
@@ -44,9 +44,9 @@ NOTES (metadata, string) are some specific notes about the nerual network base.
 %% ¡props!
 
 %%% ¡prop!
-BATCH (parameter, scalar) is the size of the mini-batch to use for each training iteration.
-%%%% ¡default!
-8
+D (data, item) is the dataset to train the neural network model.
+%%%% ¡settings!
+'NNDataset'
 
 %%% ¡prop!
 EPOCHS (parameter, scalar) is the maximum number of epochs.
@@ -54,14 +54,14 @@ EPOCHS (parameter, scalar) is the maximum number of epochs.
 20
 
 %%% ¡prop!
+BATCH (parameter, scalar) is the size of the mini-batch used for each training iteration.
+%%%% ¡default!
+8
+
+%%% ¡prop!
 SHUFFLE (parameter, option) is an option for data shuffling.
 %%%% ¡settings!
 {'once' 'never' 'every-epoch'}
-
-%%% ¡prop!
-VERBOSE (parameter, logical) is an indicator to display training progress information.
-%%%% ¡default!
-false
 
 %%% ¡prop!
 SOLVER (parameter, option) is an option for the solver.
@@ -69,31 +69,48 @@ SOLVER (parameter, option) is an option for the solver.
 {'adam' 'sgdm' 'rmsprop'}
 
 %%% ¡prop!
-PLOT_TRAINING (parameter, logical) is an option for the plot of training-progress.
-%%%% ¡default!
-false
-
-%%% ¡prop!
-D (data, item) is the dataset to train the neural network model.
-%%%% ¡settings!
-'NNDataset'
-
-%%% ¡prop!
-DATA_FORMAT (parameter, string) specifies the format of the data that flows through the neural network model.
-
-%%% ¡prop!
-DATA_CONSTRUCT (query, cell) constructs the data based on the data format that flows through the neural network model.
-
-%%% ¡prop!
-RESPONSE_CONSTRUCT (query, cell) constructs the responses based on the data format that flows through the neural network model.
-
-%%% ¡prop!
 MODEL (result, net) is a trained neural network model with the given dataset.
 %%%% ¡calculate!
 value = network();
 
 %%% ¡prop!
+INPUTS (query, matrix) constructs the cell array of the data.
+%%%% ¡calculate!
+% % % 
+
+%%% ¡prop!
+TARGETS (query, cell) constructs the cell array of the targets.
+%%%% ¡calculate!
+% % %
+
+%%% ¡prop!
 TRAIN (query, empty) trains the neural network model with the given dataset.
 %%%% ¡calculate!
 nn.memorize('MODEL');
-value = {};
+value = [];
+
+%%% ¡prop!
+VERBOSE (parameter, logical) is an indicator to display training progress information.
+%%%% ¡default!
+false
+
+%%% ¡prop!
+PLOT_TRAINING (parameter, logical) determines whether to plot the training progress.
+%%%% ¡default!
+false
+
+%%% ¡prop!
+PREDICT (query, cell) returns the prediction of the trained neural network for a dataset.
+%%%% ¡calculate!
+% PREDICTIONS = nn.get('PREDICT', D) returns a cell array with the
+%  predictions for all data points in dataset D.
+if isempty(varargin)
+    value = {};
+    return
+end
+d = varargin{1};
+
+net = nn.get('MODEL');
+predictions = net.predict();
+
+value = predictions;
