81104f26c8fbdb619dcdd00e70304234bd5f69b1
Yu-Wei Chang <hejyuwei@gmail.com>
2023-08-15 16:05:37 +0200

    init cross validation element and its derived ones

--- braph2genesis/neuralnetworks/_NNRegressor_CrossValidation.gen.m
+++ braph2genesis/neuralnetworks/_NNRegressor_CrossValidation.gen.m
@@ -1,12 +1,15 @@
 %% ¡header!
-NNRegressor_CrossValidation < NNCrossValidation (nncv, neural network cross-validation) is a process for evaluating neural network regressors using cross-validation.
+NNRegressor_CrossValidation < NNCrossValidation (nncv, neural network) comprises a neural network model with a dataset.
 
 %%% ¡description!
-A cross validation for regressors (NNRegressor_CrossValidation) is a process that facilitates the evaluation of neural network regressors using cross-validation. 
- It involves splitting a dataset into multiple subsets (folds), training the model on some folds while validating on others, and then repeating the process for all combinations of folds. 
- This helps in assessing the generalization performance of the model and detecting overfitting.
+A neural network base (NNBase) comprises a neural network model with a specific dataset.
+ Instances of this class should not be created. Use one of its subclasses instead.
 
-To train all the neural networks for all folds, use: nncv.get('TRAIN')
+Its subclasses need to implement the props MODEL, INPUTS and TARGETS.
+ Each subclass is typically optimized to work with the subclasses of NNDataPoint in the prop DP_CLASSES.
+
+To train a neural network nn use: nn.get('TRAIN')
+To obtain the prediction on a dataset D use: predictions = nn.get('PREDICT', D)
 
 %%% ¡seealso!
 NNDataPoint, NNDataset, NNEvaluator
@@ -14,207 +17,46 @@ NNDataPoint, NNDataset, NNEvaluator
 %% ¡props_update!
 
 %%% ¡prop!
-NAME (constant, string) is the name of the cross-validation.
+NAME (constant, string) is the name of the nerual network base.
 %%%% ¡default!
 'NNRegressor_CrossValidation'
 
 %%% ¡prop!
-DESCRIPTION (constant, string) is the description of the cross-validation.
+DESCRIPTION (constant, string) is the description of the nerual network base.
 %%%% ¡default!
-'A cross validation for regressors (NNRegressor_CrossValidation) is a process that facilitates the evaluation of neural network regressors using cross-validation. It involves splitting a dataset into multiple subsets (folds), training the model on some folds while validating on others, and then repeating the process for all combinations of folds. This helps in assessing the generalization performance of the model and detecting overfitting.'
+'A neural network base (NNBase) comprises a neural network model with a specific dataset. Instances of this class should not be created. Use one of its subclasses instead. Its subclasses need to implement the props MODEL, INPUTS and TARGETS.'
 
 %%% ¡prop!
-TEMPLATE (parameter, item) is the template of the nerual cross-validation.
+TEMPLATE (parameter, item) is the template of the nerual network base.
 %%%% ¡settings!
 'NNRegressor_CrossValidation'
 
 %%% ¡prop!
-ID (data, string) is a few-letter code for the cross-validation.
+ID (data, string) is a few-letter code for the nerual network base.
 %%%% ¡default!
 'NNRegressor_CrossValidation ID'
 
 %%% ¡prop!
-LABEL (metadata, string) is an extended label of the cross-validation.
+LABEL (metadata, string) is an extended label of the nerual network base.
 %%%% ¡default!
 'NNRegressor_CrossValidation label'
 
 %%% ¡prop!
-NOTES (metadata, string) are some specific notes about the cross-validation.
+NOTES (metadata, string) are some specific notes about the nerual network base.
 %%%% ¡default!
 'NNRegressor_CrossValidation notes'
 
 %%% ¡prop!
-NN_LIST (result, itemlist) contains the neural network models corresponding to k folds.
-%%%% ¡calculate!
-d_list = nncv.get('D_LIST');
-
-if isempty(d_list)
-    value = {};
-else
-    for i = 1:length(d_list)
-        d_training_set{i} = d_list;
-        d_training_set{i}(i) = [];  % Exclude the i-th element
-        d_training_set{i} = NNDatasetCombine('D_LIST', d_training_set{i}).get('D');
-    end
-
-    d_training_set = d_training_set';
-
-    nn_template = NNRegressorMLP( ...
-        'EPOCHS', nncv.get('EPOCHS'), ...
-        'BATCH', nncv.get('BATCH'), ...
-        'SHUFFLE', nncv.get('SHUFFLE'), ...
-        'SOLVER', nncv.get('SOLVER'), ...
-        'VERBOSE', nncv.get('VERBOSE'), ...
-        'PLOT_TRAINING', nncv.get('PLOT_TRAINING'));
-
-    value = cellfun(@(d) NNRegressorMLP( ...
-        'TEMPLATE', nn_template, 'D', d), ...
-        d_training_set, 'UniformOutput', false);
-end
+D (data, item) is the dataset to train the neural network model.
+%%%% ¡settings!
+'NNDataset'
 
 %%% ¡prop!
-EVALUATOR_LIST (result, itemlist) contains the evaluators corresponding to k folds.
-%%%% ¡calculate!
-d_list = nncv.get('D_LIST');
-nn_list = nncv.get('NN_LIST');
-value = cellfun(@(d, nn) NNRegressor_Evaluator('D', d, 'NN', nn), ...
-    d_list, nn_list, 'UniformOutput', false);
-
-%% ¡props!
+NN_LIST (data, itemlist) is the neural network list.
+%%%% ¡settings!
+'NNBase'
 
 %%% ¡prop!
-AV_CORR (result, rvector) provides the metric of the correlation of coefficients.
-%%%% ¡calculate!
-e_list = nncv.get('EVALUATOR_LIST');
-
-value = cellfun(@(e) e.get('CORR'), ...
-    e_list, 'UniformOutput', false);
-
-if isempty(value)
-    value = [];
-else
-    value = mean(cell2mat(value), 1);
-end
-
-%%% ¡prop!
-AV_DET (result, rvector) provides the coefficient of determination, a measure showing how well the predictions are replicated by the model.
-%%%% ¡calculate!
-e_list = nncv.get('EVALUATOR_LIST');
-
-value = cellfun(@(e) e.get('DET'), ...
-    e_list, 'UniformOutput', false);
-
-if isempty(value)
-    value = [];
-else
-    value = mean(cell2mat(value), 1);
-end
-
-%%% ¡prop!
-AV_MAE (result, rvector) provides the metric of the mean absolute error.
-%%%% ¡calculate!
-e_list = nncv.get('EVALUATOR_LIST');
-
-value = cellfun(@(e) e.get('MAE'), ...
-    e_list, 'UniformOutput', false);
-
-if isempty(value)
-    value = [];
-else
-    value = mean(cell2mat(value), 1);
-end
-
-%%% ¡prop!
-AV_MSE (result, rvector) provides the metric of the mean squared error.
-%%%% ¡calculate!
-e_list = nncv.get('EVALUATOR_LIST');
-
-value = cellfun(@(e) e.get('MSE'), ...
-    e_list, 'UniformOutput', false);
-
-if isempty(value)
-    value = [];
-else
-    value = mean(cell2mat(value), 1);
-end
-
-%%% ¡prop!
-AV_RMSE (result, rvector) provides the metric of the root mean squared error.
-%%%% ¡calculate!
-e_list = nncv.get('EVALUATOR_LIST');
-
-value = cellfun(@(e) e.get('RMSE'), ...
-    e_list, 'UniformOutput', false);
-
-if isempty(value)
-    value = [];
-else
-    value = mean(cell2mat(value), 1);
-end
-
-%%% ¡prop!
-AV_PERM_FEATURE_IMPORTANCE (result, cell) averages the permutation feature importances across k folds.
-%%%% ¡calculate!
-value = {};
-
-%% ¡tests!
-
-%%% ¡test!
-%%%% ¡name!
-evaluate a regressor cross-validation with the example data
-%%%% ¡code!
-% ensure the example data is generated
-if ~isfile([fileparts(which('NNDataPoint_CON_REG')) filesep 'Example data NN REG CON XLS' filesep 'atlas.xlsx'])
-    test_NNDataPoint_CON_REG % create example files
-end
-
-% Load BrainAtlas
-im_ba = ImporterBrainAtlasXLS( ...
-    'FILE', [fileparts(which('NNDataPoint_CON_REG')) filesep 'Example data NN REG CON XLS' filesep 'atlas.xlsx'], ...
-    'WAITBAR', true ...
-    );
-
-ba = im_ba.get('BA');
-
-% Load Groups of SubjectCON
-im_gr = ImporterGroupSubjectCON_XLS( ...
-    'DIRECTORY', [fileparts(which('NNDataPoint_CON_REG')) filesep 'Example data NN REG CON XLS' filesep 'CON_Group_XLS'], ...
-    'BA', ba, ...
-    'WAITBAR', true ...
-    );
-
-gr = im_gr.get('GR');
-
-% create a item list of NNDataPoint_CON_REG
-it_list = cellfun(@(x) NNDataPoint_CON_REG( ...
-    'ID', x.get('ID'), ...
-    'SUB', x, ...
-    'TARGET_IDS', x.get('VOI_DICT').get('KEYS')), ...
-    gr.get('SUB_DICT').get('IT_LIST'), ...
-    'UniformOutput', false);
-
-% create a NNDataPoint_CON_REG DICT
-dp_list = IndexedDictionary(...
-        'IT_CLASS', 'NNDataPoint_CON_REG', ...
-        'IT_LIST', it_list ...
-        );
-
-% create a NNData containing the NNDataPoint_CON_REG DICT
-d = NNDataset( ...
-    'DP_CLASS', 'NNDataPoint_CON_REG', ...
-    'DP_DICT', dp_list ...
-    );
-
-kfolds = 3;
-nncv = NNRegressor_CrossValidation('KFOLDS', kfolds, 'D', d);
-
-nn_list = nncv.get('NN_LIST');
-assert(length(nn_list) == kfolds, ...
-    [BRAPH2.STR ':NNRegressor_CrossValidation:' BRAPH2.FAIL_TEST], ...
-    'NNRegressor_CrossValidation does not calculate the neural network list correctly.' ...
-    )
-e_list = nncv.get('EVALUATOR_LIST');
-assert(length(e_list) == kfolds, ...
-    [BRAPH2.STR ':NNRegressor_CrossValidation:' BRAPH2.FAIL_TEST], ...
-    'NNRegressor_CrossValidation does not calculate the evaluator list correctly.' ...
-    )
+NNEvaluator_LIST (data, itemlist) is the neural network evaluator list.
+%%%% ¡settings!
+'NNEvaluator'
--- braph2genesis/src/nn/_NNCrossValidation.gen.m
+++ braph2genesis/src/nn/_NNCrossValidation.gen.m
@@ -1,178 +1,69 @@
 %% ¡header!
-NNCrossValidation < ConcreteElement (nncv, neural network cross-validation) is a process for evaluating neural network models using cross-validation.
+NNCrossValidation < ConcreteElement (nncv, neural network) comprises a neural network model with a dataset.
 
 %%% ¡description!
-A cross validation (NNCrossValidation) is a process that facilitates the evaluation of neural network models using cross-validation. 
- It involves splitting a dataset into multiple subsets (folds), training the model on some folds while validating on others, and then repeating the process for all combinations of folds. 
- This helps in assessing the generalization performance of the model and detecting overfitting.
+A neural network base (NNBase) comprises a neural network model with a specific dataset.
+ Instances of this class should not be created. Use one of its subclasses instead.
 
-To train all the neural networks for all folds, use: nncv.get('TRAIN')
+Its subclasses need to implement the props MODEL, INPUTS and TARGETS.
+ Each subclass is typically optimized to work with the subclasses of NNDataPoint in the prop DP_CLASSES.
+
+To train a neural network nn use: nn.get('TRAIN')
+To obtain the prediction on a dataset D use: predictions = nn.get('PREDICT', D)
 
 %%% ¡seealso!
-NNDataset, NNEvaluator, NNBase
+NNDataPoint, NNDataset, NNEvaluator
 
 %% ¡props_update!
 
 %%% ¡prop!
-NAME (constant, string) is the name of the cross-validation.
+NAME (constant, string) is the name of the nerual network base.
 %%%% ¡default!
 'NNCrossValidation'
 
 %%% ¡prop!
-DESCRIPTION (constant, string) is the description of the cross-validation.
+DESCRIPTION (constant, string) is the description of the nerual network base.
 %%%% ¡default!
-'A cross validation (NNCrossValidation) is a process that facilitates the evaluation of neural network models using cross-validation. It involves splitting a dataset into multiple subsets (folds), training the model on some folds while validating on others, and then repeating the process for all combinations of folds. This helps in assessing the generalization performance of the model and detecting overfitting.'
+'A neural network base (NNBase) comprises a neural network model with a specific dataset. Instances of this class should not be created. Use one of its subclasses instead. Its subclasses need to implement the props MODEL, INPUTS and TARGETS.'
 
 %%% ¡prop!
-TEMPLATE (parameter, item) is the template of the cross-validation.
+TEMPLATE (parameter, item) is the template of the nerual network base.
 %%%% ¡settings!
 'NNCrossValidation'
 
 %%% ¡prop!
-ID (data, string) is a few-letter code for the cross-validation.
+ID (data, string) is a few-letter code for the nerual network base.
 %%%% ¡default!
 'NNCrossValidation ID'
 
 %%% ¡prop!
-LABEL (metadata, string) is an extended label of the cross-validation.
+LABEL (metadata, string) is an extended label of the nerual network base.
 %%%% ¡default!
 'NNCrossValidation label'
 
 %%% ¡prop!
-NOTES (metadata, string) are some specific notes about the cross-validation.
+NOTES (metadata, string) are some specific notes about the nerual network base.
 %%%% ¡default!
 'NNCrossValidation notes'
     
 %% ¡props!
 
 %%% ¡prop!
-KFOLDS (data, scalar) is the number of folds.
+KFOLD (data, scalar) is the number of folds.
 %%%% ¡default!
 5
 
 %%% ¡prop!
-SPLIT (data, cell) is a cell containing the ratio numbers or the vectors stating which datapoints belong to the splitted neural network datasets.
-%%%% ¡postprocessing!
-split = nncv.get('SPLIT');
-kfold = nncv.get('KFOLDS');
-d = nncv.get('D');
-if isempty(split) && d.get('DP_DICT').get('LENGTH') > kfold
-    nncv.set('SPLIT', repmat({1 / kfold}, 1, kfold));
-end
-
-%%% ¡prop!
-D (data, item) is the dataset to be cross-validated.
+D (data, item) is the dataset to train the neural network model.
 %%%% ¡settings!
 'NNDataset'
 
 %%% ¡prop!
-DSP (result, item) is a dataset splitter.
+NN_LIST (data, itemlist) is the neural network list.
 %%%% ¡settings!
-'NNDatasetSplit'
-%%%% ¡calculate!
-d = nncv.get('D');
-value = NNDatasetSplit('D', d, 'SPLIT', nncv.get('SPLIT'));
+'NNBase'
 
 %%% ¡prop!
-D_LIST (result, itemlist) contains the split datasets corresponding to the k folds.
-%%%% ¡calculate!
-d = nncv.get('D');
-if d.get('DP_DICT').get('LENGTH') == 0
-    value = {};
-else
-    value = nncv.get('DSP').get('D_LIST');
-end
-
-%%% ¡prop!
-NN_LIST (result, itemlist) contains the neural network models corresponding to the k folds.
-
-%%% ¡prop!
-EVALUATOR_LIST (result, itemlist) contains the evaluators corresponding to the k folds.
-
-%%% ¡prop!
-EPOCHS (parameter, scalar) is the maximum number of epochs.
-%%%% ¡default!
-20
-
-%%% ¡prop!
-BATCH (parameter, scalar) is the size of the mini-batch used for each training iteration.
-%%%% ¡default!
-8
-
-%%% ¡prop!
-SHUFFLE (parameter, option) is an option for data shuffling.
+NNEvaluator_LIST (data, itemlist) is the neural network evaluator list.
 %%%% ¡settings!
-{'once' 'never' 'every-epoch'}
-
-%%% ¡prop!
-SOLVER (parameter, option) is an option for the solver.
-%%%% ¡settings!
-{'adam' 'sgdm' 'rmsprop'}
-
-%%% ¡prop!
-VERBOSE (parameter, logical) is an indicator to display training progress information.
-%%%% ¡default!
-false
-
-%%% ¡prop!
-PLOT_TRAINING (parameter, option) determines whether to plot the training progress.
-%%%% ¡settings!
-{'none' 'training-progress'}
-
-%%% ¡prop!
-TRAIN (query, empty) trains all neural network models for all folds.
-%%%% ¡calculate!
-nn_list = nncv.memorize('NN_LIST');
-for i = 1:1:length(nn_list)
-    nn_list{i}.memorize('MODEL');
-end
-value = [];
-
-%% ¡tests!
-
-%%% ¡test!
-%%%% ¡name!
-Creating a cross-validation object
-%%%% ¡code!
-% create a NNData containing NNDataPoints 
-num_dp = 9; 
-dp_ids = cell(1, num_dp);
-
-for i = 1:num_dp
-    dp_ids{i} = ['dp_', num2str(i)];
-end
-
-d = NNDataset('DP_DICT', IndexedDictionary(...
-    'IT_CLASS', 'NNDataPoint', ...
-    'IT_LIST',  cellfun(@(x) NNDataPoint('ID', x), ...
-    dp_ids(1:num_dp), 'UniformOutput', false) ...
-    ));
-
-% create the cross-validation process
-nncv = NNCrossValidation('D', d, 'KFOLDS', 5);
-
-% Check whether the number of datapoint from all d_list matches
-d_list = nncv.get('D_LIST');
-num_dp_from_nncv = 0;
-for i = 1:length(d_list)
-    num_dp_from_nncv = num_dp_from_nncv + d_list{i}.get('DP_DICT').get('LENGTH');
-end
-assert(num_dp_from_nncv == num_dp, ...
-    [BRAPH2.STR ':NNCrossValidation:' BRAPH2.FAIL_TEST], ...
-    'NNCrossValidation does not split the dataset correctly.' ...
-    )
-
-% create the cross-validation process with specifying the datapoints in each
-% fold
-nncv = NNCrossValidation('D', d, 'KFOLDS', 5, 'SPLIT', {[1 2], [3 4], [5 6], [7 8], [9]});
-
-% Check whether the number of datapoint in each fold matches
-split = nncv.get('SPLIT');
-d_list = nncv.get('D_LIST');
-for i = 1:length(split)
-    check(i) = d_list{i}.get('DP_DICT').get('LENGTH') == length(split{i});
-end
-assert(all(check), ...
-    [BRAPH2.STR ':NNCrossValidation:' BRAPH2.FAIL_TEST], ...
-    'NNCrossValidation does not split the dataset correctly.' ...
-    )
+'NNEvaluator'
