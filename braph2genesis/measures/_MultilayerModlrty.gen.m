820fafe6f8b60a4aa4039aebbde705b844bdb312
JW.S <jiaweisun0512@gmail.com>
2023-09-06 13:24:41 +0200

    update

---
+++ braph2genesis/measures/_MultilayerModlrty.gen.m
@@ -0,0 +1,138 @@
+%% ¡header!
+MultilayerModlrty < MultilayerCommunity  (m, multilayer modularity) is the graph multilayer modularity.
+
+%%% ¡description!
+The multilayer modularity of a multilayer graph is the multilayer 
+quality function Q of the resulting partition of the multilayer network.  
+
+%% ¡layout!
+
+%%% ¡prop!
+%%%% ¡id!
+MultilayerModlrty.ID
+%%%% ¡title!
+Measure ID
+
+%%% ¡prop!
+%%%% ¡id!
+MultilayerModlrty.LABEL
+%%%% ¡title!
+Measure NAME
+
+%%% ¡prop!
+%%%% ¡id!
+MultilayerModlrty.G
+%%%% ¡title!
+Graph
+
+%%% ¡prop!
+%%%% ¡id!
+MultilayerModlrty.M
+%%%% ¡title!
+MultilayerModlrty
+
+%%% ¡prop!
+%%%% ¡id!
+MultilayerModlrty.PFM
+%%%% ¡title!
+Measure Plot
+
+%%% ¡prop!
+%%%% ¡id!
+MultilayerModlrty.NOTES
+%%%% ¡title!
+Measure NOTES
+
+%%% ¡prop!
+%%%% ¡id!
+MultilayerModlrty.COMPATIBLE_GRAPHS
+%%%% ¡title!
+Compatible Graphs
+
+%% ¡props_update!
+
+%%% ¡prop!
+NAME (constant, string) is the name of the multilayer modularity.
+%%%% ¡default!
+'MultilayerModlrty'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the multilayer modularity.
+%%%% ¡default!
+'The multilayer modularity is the homogeneity of the number of inward neighbours of a node across the layers.'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the multilayer modularity.
+%%%% ¡settings!
+'MultilayerModlrty'
+
+%%% ¡prop!
+ID (data, string) is a few-letter code of the multilayer modularity.
+%%%% ¡default!
+'MultilayerModlrty ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the multilayer modularity.
+%%%% ¡default!
+'MultilayerModlrty label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the multilayer modularity.
+%%%% ¡default!
+'MultilayerModlrty notes'
+
+%%% ¡prop!
+SHAPE (constant, scalar) is the measure shape __Measure.NODAL__.
+%%%% ¡default!
+Measure.GLOBAL
+
+%%% ¡prop!
+SCOPE (constant, scalar) is the measure scope __Measure.UNILAYER__.
+%%%% ¡default!
+Measure.SUPERGLOBAL
+
+%%% ¡prop!
+PARAMETRICITY (constant, scalar) is the parametricity of the measure __Measure.NONPARAMETRIC__.
+%%%% ¡default!
+Measure.NONPARAMETRIC
+
+%%% ¡prop!
+COMPATIBLE_GRAPHS (constant, classlist) is the list of compatible graphs.
+%%%% ¡default!
+{'MultiplexBU'} ;%TBE % % % add any missing tests
+
+%%% ¡prop!
+M (result, cell) is the multilayer modularity.
+%%%% ¡calculate!
+multilayer_community_structure = calculateValue@MultilayerCommunity(m, prop);
+Q = 0;
+if ~isempty(cell2mat(multilayer_community_structure))
+    Q = m.get('QUALITY_FUNCTION');
+end
+multilayer_modularity = {Q};  % assign normalized quality function
+value = multilayer_modularity;
+
+%% ¡tests!
+
+%%% ¡excluded_props!
+[MultilayerModlrty.PFM]
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexBU
+%%%% ¡probability!
+.01
+%%%% ¡code!
+A = rand(5, 5);
+B = {A A};
+g = MultiplexBU('B', B);
+
+m_outside_g = MultilayerModlrty('G', g);
+assert(~isempty(m_outside_g.get('M')), ...
+    [BRAPH2.STR ':MultilayerModlrty:' BRAPH2.FAIL_TEST], ...
+    [class(m_outside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+m_inside_g = g.get('MEASURE', 'MultilayerModlrty');
+assert(~isempty(m_inside_g.get('M')), ...
+    [BRAPH2.STR ':MultilayerModlrty:' BRAPH2.FAIL_TEST], ...
+    [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
--- braph2genesis/measures/_Participation.gen.m
+++ braph2genesis/measures/_Participation.gen.m
@@ -3,53 +3,55 @@ Participation < Measure (m, participation) is the graph participation.
 
 %%% ¡description!
 The participation of a node is the ratio of edges that a node forms within 
-<<<<<<< Updated upstream
 a single layer community to the total number of edges that forms within the whole single layer graph.
 
-%%% ¡shape!
-shape = Measure.NODAL;
+%% ¡layout!
 
-%%% ¡scope!
-scope = Measure.UNILAYER;
+%%% ¡prop!
+%%%% ¡id!
+Participation.ID
+%%%% ¡title!
+Measure ID
 
-%%% ¡parametricity!
-parametricity = Measure.NONPARAMETRIC;
+%%% ¡prop!
+%%%% ¡id!
+Participation.LABEL
+%%%% ¡title!
+Measure NAME
 
-%%% ¡compatible_graphs!
-GraphBD
-GraphBU
-MultigraphBUD
-MultigraphBUT
-GraphWD
-GraphWU
-MultiplexBD
-MultiplexBU
-MultiplexBUD
-MultiplexBUT
-MultiplexWD
-MultiplexWU
+%%% ¡prop!
+%%%% ¡id!
+Participation.G
+%%%% ¡title!
+Graph
 
-%% ¡props!
-%%% ¡prop! 
-CI (parameter, MATRIX) 
-%%%% ¡default!
-1
+%%% ¡prop!
+%%%% ¡id!
+Participation.M
+%%%% ¡title!
+Participation
 
-%%% ¡prop! 
-rule (parameter, OPTION) 
-%%%% ¡settings!
-{'in' 'out' }
-%%%% ¡default!
-'out'
-=======
- a single layer community to the total number of edges that forms within the whole single layer graph.
->>>>>>> Stashed changes
+%%% ¡prop!
+%%%% ¡id!
+Participation.PFM
+%%%% ¡title!
+Measure Plot
+
+%%% ¡prop!
+%%%% ¡id!
+Participation.NOTES
+%%%% ¡title!
+Measure NOTES
+
+%%% ¡prop!
+%%%% ¡id!
+Participation.COMPATIBLE_GRAPHS
+%%%% ¡title!
+Compatible Graphs
 
 %% ¡props_update!
 
 %%% ¡prop!
-<<<<<<< Updated upstream
-=======
 NAME (constant, string) is the name of the participation.
 %%%% ¡default!
 'Participation'
@@ -82,7 +84,7 @@ NOTES (metadata, string) are some specific notes about the participation.
 %%% ¡prop!
 SHAPE (constant, scalar) is the measure shape __Measure.NODAL__.
 %%%% ¡default!
-Measure.GLOBAL
+Measure.NODAL
 
 %%% ¡prop!
 SCOPE (constant, scalar) is the measure scope __Measure.UNILAYER__.
@@ -97,23 +99,13 @@ Measure.NONPARAMETRIC
 %%% ¡prop!
 COMPATIBLE_GRAPHS (constant, classlist) is the list of compatible graphs.
 %%%% ¡default!
-{'GraphBD' 'GraphBU' 'MultigraphBUT''MultiplexBU' } ;%TBE % % % add any missing tests
+{'GraphBD' 'GraphBU' 'MultigraphBUT' 'MultiplexBU'} ;%TBE % % % add any missing tests
 
 %%% ¡prop!
->>>>>>> Stashed changes
 M (result, cell) is the participation.
 %%%% ¡calculate!
 g = m.get('G'); % graph from measure class
 A = g.get('A'); % adjacency matrix (for graph) or 2D-cell array (for multigraph, multiplex, etc.)
-<<<<<<< Updated upstream
-[l, ls] = g.layernumber();
-N = g.nodenumber();
-
-participation = cell(l, 1);        
-directionality_type =  g.getDirectionalityType(l);
-connectivity_type =  g.getConnectivityType(l);
-if ls(1) == 1
-=======
 L = g.get('LAYERNUMBER');
 N = g.get('NODENUMBER');
 ls = g.get('PARTITIONS');
@@ -122,17 +114,12 @@ participation = cell(L, 1);
 directionality_type =   g.get('DIRECTIONALITY_TYPE', L);
 connectivity_type =  g.get('CONNECTIVITY_TYPE', L);
 if ls == 1
->>>>>>> Stashed changes
     S = CommunityStructure('G', g).get('M');
 else
-    S = MultilayerCommunityStructure('G', g).get('M');
+    S = MultilayerCommunity('G', g).get('M');
 end
-    
-<<<<<<< Updated upstream
-for li = 1:1:l
-=======
+
 for li = 1:1:L
->>>>>>> Stashed changes
     connectivity_layer = connectivity_type(li, li);
     directionality_layer = directionality_type(li, li);
     Aii = A{li, li};
@@ -142,11 +129,7 @@ for li = 1:1:L
         if directionality_layer == Graph.UNDIRECTED  % undirected graphs
             ko = Strength('G', g).get('M');
         else  % directed graphs
-<<<<<<< Updated upstream
-
             directed_participation_rule = m.get('rule');
-=======
->>>>>>> Stashed changes
             switch lower(directed_participation_rule)
                 case {'in'}  % in-strength rule                   
                     ko = InStrength('G', g).get('M');                   
@@ -158,46 +141,38 @@ for li = 1:1:L
         if directionality_layer == Graph.UNDIRECTED  % undirected graphs
             ko = Degree('G', g).get('M');
         else  % directed graphs
-<<<<<<< Updated upstream
-            
-            directed_participation_rule = m.get('rule');
-=======
->>>>>>> Stashed changes
+            directed_participation_rule = m.get('RULE');
             switch lower(directed_participation_rule)
                 case {'in'}  % in-degree rule
-                    ko = InDegree('G', g).get('M');
+                    ko = DegreeIn('G', g).get('M');
                 otherwise  % {'out'}  % out-degree rule
-                    ko = OutDegree('G', g).get('M');
+                    ko = DegreeOut('G', g).get('M');
             end
         end
     end
 
     Gc = (Aii~=0)*diag(S{li});  % neighbor community affiliation
     Kc2 = zeros(N(1), 1);  % community-specific neighbors
-
     for i=1:max(S{li})
         Kc2 = Kc2 + (sum(Aii.*(Gc==i), 2).^2);
     end
-
     participation_layer = ones(N(1), 1) - Kc2./(ko{li}.^2);
     participation_layer(~ko{li}) = 0;  % participation = 0 if for nodes with no (out)neighbors
     participation(li) = {participation_layer};
 end
-<<<<<<< Updated upstream
-
-value = participation;
-
-%% ¡tests!
-
-=======
 value = participation;
 
 %% ¡props!
 
 %%% ¡prop! 
+CI (parameter, MATRIX) 
+%%%% ¡default!
+1
+
+%%% ¡prop! 
 RULE (parameter, option)
 %%%% ¡settings!
-{'in' 'out'}
+{'in', 'out'}
 %%%% ¡default!
 'out'
 
@@ -206,11 +181,11 @@ RULE (parameter, option)
 %%% ¡excluded_props!
 [Participation.PFM]
 
-
->>>>>>> Stashed changes
 %%% ¡test!
 %%%% ¡name!
 GraphBU
+%%%% ¡probability!
+.01
 %%%% ¡code!
 
 A = [
@@ -225,15 +200,7 @@ A = [
     ];
 
 known_participation = {[0 4/9 3/8 4/9 12/25 0 0 0]'};
-
 g = GraphBU('B', A);
-<<<<<<< Updated upstream
-participation = Participation('G', g).get('M');
-
-assert(isequal(participation, known_participation), ...
-    [BRAPH2.STR ':Participation:' BRAPH2.BUG_ERR], ...
-    'Participation is not being calculated correctly for GraphBU.')
-=======
 
 m_outside_g = Participation('G', g);
 assert(isequal(m_outside_g.get('M'), known_participation), ...
@@ -244,11 +211,12 @@ m_inside_g = g.get('MEASURE', 'Participation');
 assert(isequal(m_inside_g.get('M'), known_participation), ...
     [BRAPH2.STR ':Participation:' BRAPH2.FAIL_TEST], ...
     [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
->>>>>>> Stashed changes
 
 %%% ¡test!
 %%%% ¡name!
 MultigraphBUT
+%%%% ¡probability!
+.01
 %%%% ¡code!
 
 A = [
@@ -262,18 +230,11 @@ A = [
     0 0 0 0 0 0 0 0
     ];
 
-known_participation = {...
-    [0 4/9 3/8 4/9 12/25 0 0 0]'
-    [0 0   0   0   0     0 0 0]'};
+	known_participation = {...
+	    [1-1/3 1-1/3 3/4 1-1/3 4/5 1/2 1/2 1/2]'
+	    [0 0 0 0 0 0 0 0]'};
 
 g = MultigraphBUT('B', A, 'THRESHOLDS', [0 1]);
-<<<<<<< Updated upstream
-participation = Participation('G', g).get('M');
-
-assert(isequal(participation, known_participation), ...
-    [BRAPH2.STR ':Participation:' BRAPH2.BUG_ERR], ...
-    'Participation is not being calculated correctly for MultigraphBUT.')
-=======
 
 m_outside_g = Participation('G', g);
 assert(isequal(m_outside_g.get('M'), known_participation), ...
@@ -285,13 +246,12 @@ assert(isequal(m_inside_g.get('M'), known_participation), ...
     [BRAPH2.STR ':Participation:' BRAPH2.FAIL_TEST], ...
     [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
 
->>>>>>> Stashed changes
-
 %%% ¡test!
 %%%% ¡name!
 GraphBD
+%%%% ¡probability!
+.01
 %%%% ¡code!
-
 A = [
     0 1 1 1 0 0 0 0;
     0 0 1 0 1 0 0 0;
@@ -304,16 +264,9 @@ A = [
     ];
 
 % out rule - default
-known_participation_default_out = {[0 1/2 0 1/2 0 0 0 0]'};
+known_participation = {[0 1/2 0 1/2 0 0 0 0]'};
 
 g = GraphBD('B', A);
-<<<<<<< Updated upstream
-participation = Participation('G', g).get('M');
-
-assert(isequal(participation, known_participation_default_out), ...
-    [BRAPH2.STR ':Participation:' BRAPH2.BUG_ERR], ...
-    'Participation is not being calculated correctly for GraphBD.')
-=======
 
 m_outside_g = Participation('G', g);
 assert(isequal(m_outside_g.get('M'), known_participation), ...
@@ -324,19 +277,12 @@ m_inside_g = g.get('MEASURE', 'Participation');
 assert(isequal(m_inside_g.get('M'), known_participation), ...
     [BRAPH2.STR ':Participation:' BRAPH2.FAIL_TEST], ...
     [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
->>>>>>> Stashed changes
 
 % in rule 
-known_participation_in = {[0 -1 8/9 -1 5/9 0 0 1]'};
+known_participation = {[0 -1 8/9 -1 5/9 0 0 1]'};
 
 g = GraphBD('B', A);
-<<<<<<< Updated upstream
-participation = Participation('G', g, 'rule', 'in').get('M');
 
-assert(isequal(participation, known_participation_in), ...
-    [BRAPH2.STR ':Participation:' BRAPH2.BUG_ERR], ...
-    'Participation is not being calculated correctly for GraphBD.')
-=======
 m_outside_g = Participation('G', g);
 m_outside_g.set('RULE', 'in');
 assert(isequal(m_outside_g.get('M'), known_participation), ...
@@ -348,11 +294,12 @@ m_inside_g.set('RULE', 'in');
 assert(isequal(m_inside_g.get('M'), known_participation), ...
     [BRAPH2.STR ':Participation:' BRAPH2.FAIL_TEST], ...
     [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
->>>>>>> Stashed changes
 
 %%% ¡test!
 %%%% ¡name!
 MultiplexBU
+%%%% ¡probability!
+.01
 %%%% ¡code!
 
 A11 = [
@@ -376,14 +323,7 @@ known_participation = {
                  };   
  
 g = MultiplexBU('B', A);
-<<<<<<< Updated upstream
-participation = Participation('G', g).get('M');
 
-assert(isequal(participation, known_participation), ...
-    [BRAPH2.STR ':Participation:' BRAPH2.BUG_ERR], ...
-    'Participation is not being calculated correctly for MultiplexBU.')
-
-=======
 m_outside_g = Participation('G', g);
 assert(isequal(m_outside_g.get('M'), known_participation), ...
     [BRAPH2.STR ':Participation:' BRAPH2.FAIL_TEST], ...
@@ -393,4 +333,3 @@ m_inside_g = g.get('MEASURE', 'Participation');
 assert(isequal(m_inside_g.get('M'), known_participation), ...
     [BRAPH2.STR ':Participation:' BRAPH2.FAIL_TEST], ...
     [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
->>>>>>> Stashed changes
