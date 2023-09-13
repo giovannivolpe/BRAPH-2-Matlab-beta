551eef694cbefe14ba14869bd0dac4e5f2417f13
hanzh346 <80827495+hanzh346@users.noreply.github.com>
2023-09-13 21:43:36 +0200

    change OrdMlBD to OrdMxBD

--- braph2genesis/measures/_PathLengthIn.gen.m
+++ braph2genesis/measures/_PathLengthIn.gen.m
@@ -52,7 +52,7 @@ Measure.NONPARAMETRIC
 %%% ¡prop!
 COMPATIBLE_GRAPHS (constant, classlist) is the list of compatible graphs.
 %%%% ¡default!
-{'GraphBD' 'GraphWD' 'MultiplexBD' 'MultiplexWD' 'MultilayerBD' 'OrdMlBD'}
+{'GraphBD' 'GraphWD' 'MultiplexBD' 'MultiplexWD' 'MultilayerBD' 'OrdMxBD'}
 
 %%% ¡prop!
 M (result, cell) is the cell containing the PathLengthIn.
@@ -172,7 +172,7 @@ assert(isequal(m_inside_g.get('M'), known_in_path_length), ...
 
 %%% ¡test!
 %%%% ¡name!
-OrdMlBD
+OrdMxBD
 %%%% ¡code!
 B11 = [
       0  1  1  1  0;
