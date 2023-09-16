0b5ed39fb0d87d0f647fa4145da69fb2298c50e0
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-09-16 22:55:45 +0200

    Update _AnalyzeEnsemble_CON_MP_WU.gen.m

--- braph2genesis/pipelines/connectivity multiplex/_AnalyzeEnsemble_CON_MP_WU.gen.m
+++ braph2genesis/pipelines/connectivity multiplex/_AnalyzeEnsemble_CON_MP_WU.gen.m
@@ -35,6 +35,12 @@ SUBJECT GROUP
 
 %%% ¡prop!
 %%%% ¡id!
+AnalyzeEnsemble_CON_MP_WU.GRAPH_TEMPLATE
+%%%% ¡title!
+GRAPH & MEASURE PARAMETERS
+
+%%% ¡prop!
+%%%% ¡id!
 AnalyzeEnsemble_CON_MP_WU.ME_DICT
 %%%% ¡title!
 Group-averaged MEASURES
@@ -129,6 +135,9 @@ ME_DICT (result, idict) contains the calculated measures of the graph ensemble.
 % % % pr = PPAnalyzeEnsembleMP_ME_DICT('EL', a, 'PROP', AnalyzeEnsemble_CON_MP_WU.ME_DICT, 'WAITBAR', true, varargin{:});
 
 %% ¡tests!
+
+%%% ¡excluded_props!
+[AnalyzeEnsemble_CON_MP_WU.TEMPLATE AnalyzeEnsemble_CON_MP_WU.GRAPH_TEMPLATE]
 
 %%% ¡test!
 %%%% ¡name!
