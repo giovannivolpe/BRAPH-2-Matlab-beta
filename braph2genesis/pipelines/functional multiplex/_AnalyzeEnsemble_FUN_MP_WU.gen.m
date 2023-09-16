96abbd7dbf21221bd0c298ff5222ac53fc218e5f
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-09-16 23:14:35 +0200

    Update _AnalyzeEnsemble_FUN_MP_WU.gen.m

--- braph2genesis/pipelines/functional multiplex/_AnalyzeEnsemble_FUN_MP_WU.gen.m
+++ braph2genesis/pipelines/functional multiplex/_AnalyzeEnsemble_FUN_MP_WU.gen.m
@@ -36,6 +36,12 @@ SUBJECT GROUP
 
 %%% ¡prop!
 %%%% ¡id!
+AnalyzeEnsemble_FUN_MP_WU.GRAPH_TEMPLATE
+%%%% ¡title!
+GRAPH & MEASURE PARAMETERS
+
+%%% ¡prop!
+%%%% ¡id!
 AnalyzeEnsemble_FUN_MP_WU.REPETITION
 %%%% ¡title!
 REPETITION TIME [s]
@@ -216,6 +222,9 @@ Correlation.NEGATIVE_WEIGHT_RULE_LIST
 Correlation.NEGATIVE_WEIGHT_RULE_LIST{1}
 
 %% ¡tests!
+
+%%% ¡excluded_props!
+[AnalyzeEnsemble_FUN_MP_WU.TEMPLATE AnalyzeEnsemble_FUN_MP_WU.GRAPH_TEMPLATE]
 
 %%% ¡test!
 %%%% ¡name!
