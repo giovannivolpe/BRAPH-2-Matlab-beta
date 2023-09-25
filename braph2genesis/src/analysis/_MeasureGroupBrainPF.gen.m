58ec226607b28f12cc6c726f76203cf137c6d739
egolol <egolol87@gmail.com>
2023-09-25 14:21:42 +0200

    classes
    first commit, before compilation

---
+++ braph2genesis/src/analysis/_MeasureGroupBrainPF.gen.m
@@ -0,0 +1,191 @@
+%% ¡header!
+MeasureGroupBrainPF < BrainAtlasPF (pf, panel figure for measure group brain) is a plot of a measure group brain.
+
+%%% ¡description!
+A Panel Figure for a Brain Measure of Analyze Group (MeasureGroupBrainPF) manages the plot of the measure
+ ploted over the brain.  
+MeasureGroupBrainPF utilizes the surface created from BrainAtlasPF to integrate 
+ the meaure into the brain regions.
+
+%%% ¡seealso!
+BrainAtlas, BrainSurface, BrainAtlasPF
+
+%% ¡layout!
+
+%%% ¡prop!
+%%%% ¡id!
+MeasureGroupBrainPF.ID
+%%%% ¡title!
+Meaure Group Brain FigureID
+
+%%% ¡prop!
+%%%% ¡id!
+MeasureGroupBrainPF.LABEL
+%%%% ¡title!
+Meaure Group Brain FigureNAME
+
+%%% ¡prop!
+%%%% ¡id!
+MeasureGroupBrainPF.WAITBAR
+%%%% ¡title!
+WAITBAR ON/OFF
+
+%%% ¡prop!
+%%%% ¡id!
+MeasureGroupBrainPF.NOTES
+%%%% ¡title!
+Meaure Group Brain NOTES
+
+%%% ¡prop!
+%%%% ¡id!
+MeasureGroupBrainPF.SHOWMEASURE
+%%%% ¡title!
+Meaure Group Brain SHOWMEASURE
+
+%% ¡props_update!
+
+%%% ¡prop!
+ELCLASS (constant, string) is the class of the panel figure for measure group brain.
+'MeasureGroupBrainPF'
+
+%%% ¡prop!
+NAME (constant, string) is the name of the panel figure for measure group brain.
+%%%% ¡default!
+'Panel Figure for Measure Group Brain'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the panel figure for measure group brain.
+%%%% ¡default!
+'A Panel Figure for a Brain Measure of Analyze Group (MeasureGroupBrainPF) manages the plot of the measure ploted over the brain. MeasureGroupBrainPF utilizes the surface created from BrainAtlasPF to integrate the meaure into the brain regions.'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the panel figure for measure group brain.
+%%%% ¡settings!
+'MeasureGroupBrainPF'
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the panel figure for measure group brain.
+%%%% ¡default!
+'MeasureGroupBrainPF ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the panel figure for measure group brain.
+%%%% ¡default!
+'MeasureGroupBrainPF label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the panel figure for measure group brain.
+%%%% ¡default!
+'MeasureGroupBrainPF notes'
+
+%%% ¡prop!
+DRAW (query, logical) draws the figure brain atlas.
+%%%% ¡calculate!
+value = calculateValue@BrainAtlasPF(pf, BrainAtlasPF.DRAW, varargin{:}); % also warning
+if value
+    % reset the ambient lighting
+    pf.get('ST_AMBIENT').get('SETUP')
+end
+%%%% ¡calculate_callbacks!
+function cb_show_measure(~, ~, show) % (src, event)
+    pf.set('SHOWMEASURE', show)
+end
+
+%%% ¡prop!
+DELETE (query, logical) resets the handles when the panel figure brain surface is deleted.
+%%%% ¡calculate!
+value = calculateValue@BrainAtlasPF(pf, BrainAtlasPF.DELETE, varargin{:}); % also warning
+if value
+    % do nothing
+end
+
+%%% ¡prop!
+H_TOOLS (evanescent, handlelist) is the list of panel-specific tools from the first.
+%%%% ¡calculate!
+value = calculateValue@BrainAtlasPF(pf, BrainAtlasPF.H_TOOLS, varargin{:}); % also warning
+if value
+     tool_separator_2 = uipushtool(toolbar, 'Separator', 'on', 'Visible', 'off');
+    
+    % SHOW MEASURE
+    tool_show_measure = uitoggletool(toolbar, ...
+        'Tag', 'TOOL.SHOWMEASURE', ...
+        'Separator', 'on', ...
+        'State', pf.get('SHOWMEASURE'), ...
+        'Tooltip', 'Show Measure', ...
+        'CData', imread('braph2icon16px.png'), ...
+        'OnCallback', {@cb_show_measure, true}, ...
+        'OffCallback', {@cb_show_measure, false});
+
+     value = {value{:}, ...
+         tool_separator_2, ...
+         tool_show_measure ...
+         };
+
+else
+    value = {};
+end
+
+%% ¡props!
+
+%%% ¡prop!
+SHOWMEASURE (query, logical) resets the handles when the panel figure brain surface is deleted.
+%%%% ¡default!
+false
+
+%%% ¡prop!
+M (metadata, item) is the measure.
+%%%% ¡settings!
+'Measure'
+
+%% ¡tests!
+
+%%% ¡excluded_props!
+[MeasureGroupBrainPF.PARENT MeasureGroupBrainPF.H MeasureGroupBrainPF.ST_POSITION MeasureGroupBrainPF.ST_AXIS MeasureGroupBrainPF.ST_SURFACE MeasureGroupBrainPF.ST_AMBIENT]
+
+%%% ¡warning_off!
+true
+
+%%% ¡test!
+%%%% ¡name!
+Remove Figures
+%%%% ¡code!
+warning('off', [BRAPH2.STR ':MeasureGroupBrainPF'])
+assert(length(findall(0, 'type', 'figure')) == 5)
+delete(findall(0, 'type', 'figure'))
+warning('on', [BRAPH2.STR ':MeasureGroupBrainPF'])
+
+%%% ¡test!
+%%%% ¡name!
+Basics 1
+%%%% ¡probability!
+.01
+%%%% ¡code!
+br1 = BrainRegion('ID', 'id1', 'LABEL', 'label1', 'NOTES', 'notes1', 'X', 1, 'Y', 1, 'Z', 1);
+br2 = BrainRegion('ID', 'id2', 'LABEL', 'label2', 'NOTES', 'notes2', 'X', 2, 'Y', 2, 'Z', 2);
+br3 = BrainRegion('ID', 'id3', 'LABEL', 'label3', 'NOTES', 'notes3', 'X', 3, 'Y', 3, 'Z', 3);
+br4 = BrainRegion('ID', 'id4', 'LABEL', 'label4', 'NOTES', 'notes4', 'X', 4, 'Y', 4, 'Z', 4);
+br5 = BrainRegion('ID', 'id5', 'LABEL', 'label5', 'NOTES', 'notes5', 'X', 5, 'Y', 5, 'Z', 5);
+br6 = BrainRegion('ID', 'id6', 'LABEL', 'label6', 'NOTES', 'notes6', 'X', 6, 'Y', 6, 'Z', 6);
+items = {br1, br2, br3, br4, br5, br6};
+idict = IndexedDictionary( ...
+    'ID', 'idict', ...
+    'IT_CLASS', 'BrainRegion', ...
+    'IT_KEY', IndexedDictionary.getPropDefault(IndexedDictionary.IT_KEY), ...
+    'IT_LIST', items ...
+    );
+ba0 = BrainAtlas('ID', 'BA1', 'LABEL', 'brain atlas', 'NOTES', 'Notes on brain atlas.', 'BR_DICT', idict);
+pf0 = MeasureGroupBrainPF(...
+    'SURF', ImporterBrainSurfaceNV('FILE', 'human_ICBM152.nv').get('SURF'), ...
+    'BA', ba0 ...
+    );
+gui0 = GUIFig('PF', pf0, 'FILE', 'xxx sss', 'POSITION', [.1 .1 .4 .8], 'WAITBAR', true, 'CLOSEREQ', false);
+gui0.get('DRAW')
+gui0.get('SHOW')
+
+gui0_settings = gui0.memorize('GUI_SETTINGS');
+gui0_settings.set('POSITION', [0 0 .3 .5])
+gui0_settings.get('DRAW')
+gui0_settings.get('SHOW')
+
+gui0.get('CLOSE')
+
---
+++ braph2genesis/src/analysis/_MeasureGroupBrainPF_BB.gen.m
@@ -0,0 +1,92 @@
+%% ¡header!
+MeasureGroupBrainPF_BB  < MeasureGroupBrainPF (pf, panel figure for binodal bilayer measure group brain) is a plot of a binodal bilayer measure group brain.
+
+%%% ¡description!
+A Panel Figure for a Brain Measure of Analyze Group (MeasureGroupBrainPF_BB ) manages the plot of the binodal bilayer measure
+ ploted over the brain.  
+MeasureGroupBrainPF_BB  utilizes the surface created from BrainAtlasPF to integrate 
+ the binodal bilayer meaure into the brain regions.
+
+%%% ¡seealso!
+BrainAtlas, BrainSurface, BrainAtlasPF
+
+%% ¡layout!
+
+%%% ¡prop!
+%%%% ¡id!
+MeasureGroupBrainPF_BB .ID
+%%%% ¡title!
+Brain Atlas Figure ID
+
+%%% ¡prop!
+%%%% ¡id!
+MeasureGroupBrainPF_BB .LABEL
+%%%% ¡title!
+Brain Atlas Figure NAME
+
+%%% ¡prop!
+%%%% ¡id!
+MeasureGroupBrainPF_BB .WAITBAR
+%%%% ¡title!
+WAITBAR ON/OFF
+
+%%% ¡prop!
+%%%% ¡id!
+MeasureGroupBrainPF_BB .NOTES
+%%%% ¡title!
+Brain Atlas NOTES
+
+%% ¡props_update!
+
+%%% ¡prop!
+ELCLASS (constant, string) is the class of the panel figure for measure group brain.
+'MeasureGroupBrainPF_BB '
+
+%%% ¡prop!
+NAME (constant, string) is the name of the panel figure for measure group brain.
+%%%% ¡default!
+'Panel Figure for Measure Group Brain'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the panel figure for measure group brain.
+%%%% ¡default!
+'A Panel Figure for a Brain Measure of Analyze Group (MeasureGroupBrainPF_BB ) manages the plot of the binodal bilayer measure ploted over the brain. MeasureGroupBrainPF_BB  utilizes the surface created from BrainAtlasPF to integrate the binodal bilayer meaure into the brain regions.'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the panel figure for measure group brain.
+%%%% ¡settings!
+'MeasureGroupBrainPF_BB '
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the panel figure for measure group brain.
+%%%% ¡default!
+'MeasureGroupBrainPF_BB  ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the panel figure for measure group brain.
+%%%% ¡default!
+'MeasureGroupBrainPF_BB  label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the panel figure for measure group brain.
+%%%% ¡default!
+'MeasureGroupBrainPF_BB  notes'
+
+%%% ¡prop!
+DRAW (query, logical) draws the figure brain atlas.
+%%%% ¡calculate!
+value = calculateValue@MeasureGroupBrainPF(pf, MeasureGroupBrainPF_BB.DRAW, varargin{:}); % also warning
+if value
+    % reset the ambient lighting
+    pf.get('ST_AMBIENT').get('SETUP')
+end
+
+
+%%% ¡prop!
+DELETE (query, logical) resets the handles when the panel figure brain surface is deleted.
+%%%% ¡calculate!
+value = calculateValue@MeasureGroupBrainPF(pf, MeasureGroupBrainPF_BB.DELETE, varargin{:}); % also warning
+if value
+
+end
+
