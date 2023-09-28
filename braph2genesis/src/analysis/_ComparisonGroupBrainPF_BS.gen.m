ca3e06b61291651ca68be9c10356383024283cb7
bzufiriaki <blanca.zufiria.gerboles@ki.se>
2023-09-28 15:17:14 +0200

    solve test removing figures

--- braph2genesis/src/analysis/_ComparisonGroupBrainPF_BS.gen.m
+++ braph2genesis/src/analysis/_ComparisonGroupBrainPF_BS.gen.m
@@ -120,6 +120,6 @@ true
 Remove Figures
 %%%% ¡code!
 warning('off', [BRAPH2.STR ':ComparisonGroupBrainPF_BS'])
-assert(length(findall(0, 'type', 'figure')) == 1)
+assert(length(findall(0, 'type', 'figure')) == 5)
 delete(findall(0, 'type', 'figure'))
 warning('on', [BRAPH2.STR ':ComparisonGroupBrainPF_BS'])
