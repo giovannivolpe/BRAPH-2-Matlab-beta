f4eb8f586e06b7505601ac20e1a95d4de1fc69cb
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-09-28 09:58:10 +0200

    Update _ComparisonEnsembleBrainPF.gen.m

--- braph2genesis/src/analysis/_ComparisonEnsembleBrainPF.gen.m
+++ braph2genesis/src/analysis/_ComparisonEnsembleBrainPF.gen.m
@@ -132,7 +132,7 @@ Brain Region LABELS PROPERTIES
 %% ¡props_update!
 
 %%% ¡prop!
-ELCLASS (constant, string) is the class of the panel ensemble-based comparison figure on brain surface.
+ELCLASS (constant, string) is the class of % % % .
 %%%% ¡default!
 'ComparisonEnsembleBrainPF'
 
